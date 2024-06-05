0f1d613585a8e0ab025b8bf28540a021dfb4c4c0
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-05 10:59:47 -0500

    Created login page

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 const sequelize = require('./config/connection');
+const routes = require('./controllers/posts');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(routes);
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', async (req, res) => {
-    res.render('home');
-});
+
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
