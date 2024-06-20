62213d312f93b19803fcf3822363b4caa0ac458d
atengelsgjerd <atengelsgjerd@gmail.com>
2024-06-20 11:55:36 -0500

    added styling to webpage

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,15 @@
 
-const loginFormHandler = async (event) => {
+const loginFormFunction = async (event) => {
+    console.log('login form function');
     event.preventDefault();
 
     const email = document.querySelector('#email').value.trim();
     const password = document.querySelector('#password').value.trim();
-
     if (email && password) {
-        const response = await fetch('/api/users/login', {
-            method: 'POST',
-            body: JSON.stringify({email, password}),
-            headers: { 'Content-Type': 'application/json' },
-        });
-
-        if (response.ok) {
-            document.location.replace('/');
+        const response = await axios.post('/api/users/login', {email, password});   
+        if (response.status === 200) {
+            document.location.assign('/');
+            console.log('logged in');
         } else {
             alert('Failed to log in');
         }
@@ -21,5 +17,5 @@ const loginFormHandler = async (event) => {
 };
 
 document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+    .querySelector('#loginBtn')
+    .addEventListener('click', loginFormFunction);
--- public/js/signup.js
+++ public/js/signup.js
@@ -1,28 +1,20 @@
+const signUpFormFunction = async (event) => {
+    event.preventDefault();
 
-const signUpButtonEl = document.getElementById('submitBtn');
-
-async function signUpFunction(e){
-    e.preventDefault();
-    const usernameEl = document.getElementById('username').value.trim();
-    const emailEl = document.getElementById('email').value.trim();
-    const passwordEl = document.getElementById('password').value.trim();
-   
-    
-    if (usernameEl && emailEl && passwordEl) {
-        const response = await fetch('/api/users', {
-            method: 'POST',
-            body: JSON.stringify({username: usernameEl, email: emailEl, password: passwordEl}),
-            headers: { 'Content-Type' : 'application/json' },
-
-        });
-
-    if (response.ok) {
-        document.location.replace('/');
-    } else {
-        alert('Failed to sign up');
+    const username = document.querySelector('#username').value.trim();
+    const email = document.querySelector('#signUpEmail').value.trim();
+    const password = document.querySelector('#signUpPassword').value.trim();
+    if (username && email && password) {
+        const response = await axios.post('/api/users', {username, email, password});   
+        if (response.status === 200) {
+            document.location.assign('/');
+            console.log('logged in');
+        } else {
+            alert('Failed to log in');
+        }
     }
 }
 
-};
-
-signUpButtonEl.addEventListener('click', signUpFunction);
+document
+    .querySelector('#submitBtn')
+    .addEventListener('click', signUpFormFunction);
